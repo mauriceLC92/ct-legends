235f56ca8a19cb2d8176cd202df185564cbadfdb
Maurice Le Cordier <mauricelecordier@gmail.com>
2021-11-21 19:34:27 +0200

    added the navbar from tailwindui to experiment with

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import type { NextPage } from 'next'
+import { Navbar } from '../components/Navbar/Navbar'
 
 const Home: NextPage = () => {
     return (
         <div className="">
-            <main className=""></main>
+            <main className="">
+                <Navbar />
+            </main>
         </div>
     )
 }
