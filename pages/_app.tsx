07acca366b03c95a5d466406540ba082559b02e7
Maurice Le Cordier <mauricelecordier@gmail.com>
2021-11-21 18:19:36 +0200

    adds prettier and removes all initial components from the boilerplate

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>CT Legends</title>
+                <meta name="description" content="Legends of Crypto Twitter" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp
